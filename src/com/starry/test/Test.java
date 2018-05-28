363592ff94f86d7e2e4f1bffec51880f1484b876
unohacha@163.com <unohacha@163.com>
2018-05-28 10:34:59 +0800

    测试提交

--- src/com/starry/test/Test.java
+++ src/com/starry/test/Test.java
@@ -9,5 +9,9 @@ public class Test {
 	public static void main(String[] args) {
 		UserDaoImp ud=new UserDaoImp();
 		ud.userLevel("starry", "6512bd43d9caa6e02c990b0a82652dca");
+		ud.userLevel("starry1", "6512bd43d9caa6e02c990b0a82652dca");
+		ud.userLevel("starry2", "6512bd43d9caa6e02c990b0a82652dca");
+		ud.userLevel("starry3", "6512bd43d9caa6e02c990b0a82652dca");
+
 	}
 }
